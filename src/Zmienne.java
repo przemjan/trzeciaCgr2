341d33e9b0065c1e4efb74aa8f09d1919435a021
przemjan <przemjanisz2@gmail.com>
2022-10-19 08:25:35 +0200

    Initial commit

---
+++ src/Zmienne.java
@@ -0,0 +1,41 @@
+public class Zmienne {
+    public static void main(String[] args) {
+
+        int number1;
+        number1 = 13;
+
+        double number2 = 24.9;
+
+        System.out.println("Number1 ma wartość" + number1 + "tekst");
+
+        number1 = (int)number2;
+
+        System.out.println(number1);
+
+
+        int workHoursPerYear;
+        int hours = 8;
+        int days = 5;
+
+        int weeks = 7;
+
+        // komentarz
+        workHoursPerYear = hours * days * weeks;
+
+        System.out.println("Godziny: " + workHoursPerYear);
+
+
+        String pogoda = "Jest \"piękna\" pogoda\n";
+
+        System.out.println(pogoda + " to jest mój napis");
+        System.out.print(pogoda + " to\tjest\n mój napis \n");
+        System.out.print(pogoda + " to jest mój napis \n");
+
+
+        char znak = '#';
+
+        boolean slonce = false; //true
+
+
+    }
+}
